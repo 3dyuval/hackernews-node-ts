ab928d4ae5a040123bb4bb42050dac3592ab032a
Yuval Dikerman <3dyuval@gmail.com>
2023-03-18 12:16:06 +0200

    yoga running

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,14 @@
 import { execute, parse } from 'graphql'
+import { createYoga } from 'graphql-yoga'
+import { createServer } from 'http'
 import { schema } from './schema'
 
 async function main() {
-	const myQuery = parse(/* GraphQL */ `
-		query {
-			hello
-		}
-	`)
-
-	const result = await execute({
-		schema,
-		document: myQuery,
+	const yoga = createYoga({ schema })
+	const server = createServer(yoga)
+	server.listen(4000, () => {
+		console.info(`server is listening on http://localhost:4000/graphql`)
 	})
-
-	console.log(result)
 }
 
 main()
