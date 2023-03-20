e446ba614a9c8d6864cf898f86be847a1e9cbb55
Yuval Dikerman <3dyuval@gmail.com>
2023-03-20 08:53:48 +0200

    schema

--- src/schema.ts
+++ src/schema.ts
@@ -2,17 +2,44 @@ import { makeExecutableSchema } from '@graphql-tools/schema'
 
 const typeDefinitions = /* GraphQL */ `
 	type Query {
-		hello: String!
+		info: String!
+		feed: [Link!]!
+	}
+
+	type Link {
+		id: ID!
+		description: String!
+		url: String!
 	}
 `
 
+type Link = {
+	id: string
+	url: string
+	description: string
+}
+
+const links: Link[] = [
+	{
+		id: '1',
+		url: 'www',
+		description: 'link1',
+	},
+]
+
 const resolvers = {
 	Query: {
-		hello: () => 'hello world',
+		info: () => 'This is an api from Hackernews',
+		feed: () => links,
+	},
+	Link: {
+		id: (parent: Link) => parent.id,
+		description: (parent: Link) => parent.description,
+		url: (parent: Link) => parent.url,
 	},
 }
 
 export const schema = makeExecutableSchema({
-    resolvers: [resolvers],
-    typeDefs: [typeDefinitions]
+	resolvers: [resolvers],
+	typeDefs: [typeDefinitions],
 })
