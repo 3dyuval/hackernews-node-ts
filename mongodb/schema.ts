5bc57178d2e15b77e40ce332d6e25b61bfffa33e
Yuval Dikerman <3dyuval@gmail.com>
2023-03-31 08:39:55 +0300

    graphql resolver migration to mongodb

--- mongodb/schema.ts
+++ mongodb/schema.ts
@@ -6,4 +6,6 @@ dotenv.config()
 const dbURI = process.env.MONGODB_URL!
 if (!dbURI) throw new Error ('Mongo DB URI not found in environment')
 
-export const client = new MongoClient(dbURI)
+ export const client = new MongoClient(dbURI)
+
+    
--- src/schema.ts
+++ src/schema.ts
@@ -1,7 +1,6 @@
 import { makeExecutableSchema } from '@graphql-tools/schema'
 import { GraphQLError } from 'graphql'
 import type { GraphQLContext } from './context'
-import {eq} from 'drizzle-orm/expressions'
 
 const typeDefinitions = /* GraphQL */ `
 	type Query {
@@ -31,21 +30,23 @@ const resolvers = {
 	Query: {
 		info: () => 'This is an api from Hackernews',
 		feed: async (parent: unknown, args: {}, context: GraphQLContext) => {
-			return await context.db.select().from(link)
+			return await context.db.collection('Link')
 		},
 		comment: async (
 			parent: unknown,
 			args: { id: string },
 			context: GraphQLContext
 		) => {
-			return await context.db.select().from(comment).where(eq(comment.id, parseInt(args.id)))
+			return await context.db.collection('Comment')
+			// return await context.db.select().from(comment).where(eq(comment.id, parseInt(args.id)))
 		},
 		link: async (
 			parent: unknown,
 			args: { id: string },
 			context: GraphQLContext
 		) => {
-			return await context.db.select().from(comment).where(eq(comment.id, parseInt(args.id)))
+			return null
+			// return await context.db.select().from(comment).where(eq(comment.id, parseInt(args.id)))
 		},
 	},
 	Mutation: {
@@ -54,38 +55,44 @@ const resolvers = {
 			args: { description: string; url: string },
 			context: GraphQLContext
 		) {
-			return await context.db.insert(link).values({
-				description: args.description,
-				url: args.url
-			})
+			return null
+			// return await context.db.insert(link).values({
+			// 	description: args.description,
+			// 	url: args.url
+			// })
 		},
 		async postCommentOnLink(
 			parent: unknown,
 			args: { linkId: string; body: string },
 			context: GraphQLContext
 		  ) {
-			const newComment = await context.db.insert(comment).values({
-				body: args.body,
-				linkId: parseInt(args.linkId)
-			})
-			  .catch((e: unknown) => {
-				if (
-				  e
-				) {
-				  return Promise.reject(
-					new GraphQLError(
-					  `Cannot post comment on non-existing link with id '${args.linkId}'.`
-					)
-				  )
-				}
-				return Promise.reject(e)
-			  })
-			return newComment
+			return null
 		  }
-	  },
+			// const newComment = await context.db.insert(comment).values({
+			// 	body: args.body,
+			// 	linkId: parseInt(args.linkId)
+			// })
+		// 	  .catch((e: unknown) => {
+		// 		if (
+		// 		  e
+		// 		) {
+		// 		  return Promise.reject(
+		// 			new GraphQLError(
+		// 			  `Cannot post comment on non-existing link with id '${args.linkId}'.`
+		// 			)
+		// 		  )
+		// 		}
+		// 		return Promise.reject(e)
+		// 	  })
+		// 	return newComment
+		//   }
+	//   },
+		},
 	Link: {
-		 async comments (parent: Link, args: {}, context: GraphQLContext) {
-			return context.db.select().from(comment).where(eq(comment.linkId, parent.id))
+		 async comments (parent: any, args: {}, context: GraphQLContext) {
+			return null
+
+			// return context.db.select().from(comment).where(eq(comment.linkId, parent.id))
 		},
 	},
 }
