5f12228047cb7918cf12ae7a9696a1569808d399
Yuval Dikerman <3dyuval@gmail.com>
2023-04-01 22:03:23 +0300

    mongoose schema

--- mongodb/schema.ts
+++ mongodb/schema.ts
@@ -1,11 +1,11 @@
-import { MongoClient } from 'mongodb';
 import * as dotenv from 'dotenv'
+import mongoose from 'mongoose';
 dotenv.config()
 
 
 const dbURL = process.env.MONGODB_URL!
 if (!dbURL) throw new Error ('Mongo DB URL not found in environment')
 
- export const client = new MongoClient(dbURL)
-
-    
+const Schema = new mongoose.Schema({
+    //TODO
+})
