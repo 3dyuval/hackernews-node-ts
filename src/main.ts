ef57c8a2199a2fa94f801c670eacb0ce30a6c576
Yuval Dikerman <3dyuval@gmail.com>
2023-04-15 10:12:03 +0300

    docker stuff

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { createServer } from 'http'
 import { schema } from './schema'
 import { createContext } from './context'
 
+const PORT = process.env.PORT || 3000
 async function main() {
 	const yoga = createYoga({ schema, context: createContext })
 	const server = createServer(yoga)
-	server.listen(4000, () => {
-		console.info(`server is listening on http://localhost:4000/graphql`)
+	server.listen(PORT, () => {
+		console.info(`server is listening on http://localhost:${PORT}/graphql`)
 	})
 }
 
